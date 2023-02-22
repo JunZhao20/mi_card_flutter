1d8ab2c72bf8f3b568343ff04c500174c24d62b7
Jun Zhao <junzhao1111@gmail.com>
2023-02-22 12:14:37 +0000

    Added a SafeArea with margins and dimensions

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        // SafeArea allows widgets to within a border
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Text'),
+          ),
+        ),
       ),
     );
   }
